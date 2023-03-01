92ca29c5b03a9cf3df13a896beb38f6ea583071f
Lcuky12 <118757873+Lcuky12@users.noreply.github.com>
2023-03-01 16:19:06 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int intialValue = 5;
+            int maximumValue = 97;
+            int additionNumber = 7;
+
+            for (int i = intialValue; i < maximumValue; i += additionNumber)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
